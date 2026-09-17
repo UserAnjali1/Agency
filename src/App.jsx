e92274e2568e3284e99e23ac84176c8cf861fd0e
anjalipandey-dev <anjalipandey.dev@gmail.com>
2026-09-17 14:26:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Navbar from "./components/navbar";
+const App = () => {
+  const [theme, setTheme] = useState("light");
+  return (
+    <div className="dark:bg-black relative">
+      <Navbar theme={theme} setTheme={setTheme} />
+    </div>
+  );
+};
+
+export default App;
